5c1dcc1cb47ceee8404550af1712664f68d4f3c9
Toby Chui <tobychui@users.noreply.github.com>
2017-02-15 08:51:51 +0800

    Add files via upload
    Improved debug GUI and error report.

---
+++ GCCI/DebugConsole.vb
@@ -0,0 +1,24 @@
+﻿Imports System.IO
+
+Public Class DebugConsole
+    Dim exportpath As String = Application.StartupPath & "\log.txt"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DebugConsole_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = ""
+        Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(exportpath)
+        Dim a As String
+        Do
+            a = reader.ReadLine
+            Label1.Text += a & vbNewLine
+        Loop Until a Is Nothing
+        reader.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.Left = Form1.Left + Form1.Width
+        Me.Top = Form1.Top
+    End Sub
+End Class
--- GCCI/Form1.vb
+++ GCCI/Form1.vb
@@ -6,6 +6,9 @@ Public Class Form1
     Private MouseDownY As Integer
     Dim gccloc As String = "C:\MinGW\bin\gcc.exe"
     Dim apppath As String = Application.StartupPath & "\"
+    Dim logfile As String = Application.StartupPath & "\log.txt"
+    Dim p() As Process
+    Dim Errormsg As Boolean = False
 
 
     Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles Panel1.MouseDown
@@ -38,18 +41,21 @@ Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.AllowDrop = True
     End Sub
-    Private Sub Form1_DragDrop(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragDrop
+    Private Sub Form1_DragDrop(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragDrop, Label3.DragDrop
+        Errormsg = False
         Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
         For Each lpath In files
             'MsgBox(path)
-            Compile(lpath)
+            NewCompile(lpath)
             'MsgBox(lpath)
             Label3.Text = "Compile Finished, saved as: " & vbNewLine & apppath & Path.GetFileName(lpath).Replace(".c", ".exe")
         Next
-
+        If Errormsg = True Then
+            Label3.Text += vbNewLine & "Seems there is some error during compilation :("
+        End If
     End Sub
 
-    Private Sub Form1_DragEnter(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragEnter
+    Private Sub Form1_DragEnter(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragEnter, Label3.DragEnter
         If e.Data.GetDataPresent(DataFormats.FileDrop) Then
             e.Effect = DragDropEffects.Copy
         End If
@@ -66,12 +72,49 @@ Public Class Form1
     End Sub
 
     Private Sub Compile(filedir As String)
-        'MsgBox("""" & filedir & """" & " " & "-o" & " " & """" & apppath & Path.GetFileName(filedir).Replace(".c", ".exe") & """")
+        'MsgBox("""" & filedir & """" & " " & "-o" & " " & """" & apppath & Path.GetFileName(filedir).Replace(".c", ".exe") & """" & " > " & """" & logfile & """" & "2>&1")
+        'TextBox1.Text = """" & filedir & """" & " " & "-o" & " " & """" & apppath & Path.GetFileName(filedir).Replace(".c", ".exe") & """" & " > " & """" & logfile & """" & "2>&1"
         Dim compiler As New ProcessStartInfo
         compiler.FileName = gccloc
-        compiler.Arguments = """" & filedir & """" & " " & "-o" & " " & """" & apppath & Path.GetFileName(filedir).Replace(".c", ".exe") & """"
+        'compiler.Arguments = """" & filedir & """" & " " & "-o" & " " & """" & apppath & Path.GetFileName(filedir).Replace(".c", ".exe") & """"
+        compiler.Arguments = """" & filedir & """" & " " & "-o" & " " & """" & apppath & Path.GetFileName(filedir).Replace(".c", ".exe") & """" & " > " & """" & logfile & """" & "2>&1"
         compiler.UseShellExecute = True
+        'compiler.RedirectStandardOutput = True
         compiler.WindowStyle = ProcessWindowStyle.Normal
-        Dim proc As Process = Process.Start(compiler)
+        Dim proc As New Process()
+        proc.StartInfo = compiler
+        proc.Start()
+        'MsgBox(sOutput)
     End Sub
+
+    Private Sub NewCompile(filedir As String)
+        'Remove the old log.txt file before it can read
+        Dim pnumber As Integer = GetProcessNumber()
+        Dim exportpath As String = Application.StartupPath & "\log.txt"
+        If My.Computer.FileSystem.FileExists(exportpath) Then
+            My.Computer.FileSystem.DeleteFile(exportpath)
+        End If
+        Shell("cmd.exe /c" & gccloc & " " & """" & filedir & """" & " " & "-o" & " " & """" & apppath & Path.GetFileName(filedir).Replace(".c", ".exe") & """" & " > " & """" & logfile & """" & "2>&1")
+        While GetProcessNumber() <> pnumber
+            'While the process is not yet ended, keep waiting
+            Threading.Thread.Sleep(300)
+            Application.DoEvents()
+        End While
+        If My.Computer.FileSystem.FileExists(exportpath) Then
+            If (File.ReadAllText(exportpath).Length <> 0) Then
+                'There is error in the compilation process
+                Dim Dc As New DebugConsole
+                Dc.Show()
+                Errormsg = True
+            Else
+                'Everthing works fine
+                My.Computer.FileSystem.DeleteFile(exportpath)
+            End If
+        End If
+
+    End Sub
+    Private Function GetProcessNumber()
+        p = Process.GetProcessesByName("cmd")
+        Return p.Count
+    End Function
 End Class
